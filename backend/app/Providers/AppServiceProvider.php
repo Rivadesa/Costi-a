793b995774a219252639ee1f978452510e06e2b3
Rivadesa <info@xeitoso.com>
2026-09-14 11:35:37 +0200

    feat: bind menu template repository

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Infrastructure\Persistence\LaravelIdempotencyStore;
+use App\Infrastructure\Persistence\LaravelMenuTemplateRepository;
 use App\Infrastructure\Persistence\LaravelOutboxStore;
 use App\Infrastructure\Persistence\LaravelTableServiceRepository;
 use App\Infrastructure\Persistence\LaravelTransactionManager;
 use Hospitality\Application\Contracts\IdempotencyStore;
+use Hospitality\Application\Contracts\MenuTemplateRepository;
 use Hospitality\Application\Contracts\OutboxStore;
 use Hospitality\Application\Contracts\TableServiceRepository;
 use Hospitality\Application\Contracts\TransactionManager;
@@ -19,6 +21,7 @@ final class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->scoped(TableServiceRepository::class, LaravelTableServiceRepository::class);
+        $this->app->scoped(MenuTemplateRepository::class, LaravelMenuTemplateRepository::class);
         $this->app->scoped(TransactionManager::class, LaravelTransactionManager::class);
         $this->app->scoped(IdempotencyStore::class, LaravelIdempotencyStore::class);
         $this->app->scoped(OutboxStore::class, LaravelOutboxStore::class);
