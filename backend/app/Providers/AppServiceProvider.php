c73e41be0ec7bc047f7fc656488006a0192552ba
Rivadesa <info@xeitoso.com>
2026-09-14 11:36:11 +0200

    feat: bind scoped dining table repository

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Infrastructure\Persistence\LaravelDiningTableRepository;
 use App\Infrastructure\Persistence\LaravelIdempotencyStore;
 use App\Infrastructure\Persistence\LaravelMenuTemplateRepository;
 use App\Infrastructure\Persistence\LaravelOutboxStore;
 use App\Infrastructure\Persistence\LaravelTableServiceRepository;
 use App\Infrastructure\Persistence\LaravelTransactionManager;
+use Hospitality\Application\Contracts\DiningTableRepository;
 use Hospitality\Application\Contracts\IdempotencyStore;
 use Hospitality\Application\Contracts\MenuTemplateRepository;
 use Hospitality\Application\Contracts\OutboxStore;
@@ -22,6 +24,7 @@ final class AppServiceProvider extends ServiceProvider
     {
         $this->app->scoped(TableServiceRepository::class, LaravelTableServiceRepository::class);
         $this->app->scoped(MenuTemplateRepository::class, LaravelMenuTemplateRepository::class);
+        $this->app->scoped(DiningTableRepository::class, LaravelDiningTableRepository::class);
         $this->app->scoped(TransactionManager::class, LaravelTransactionManager::class);
         $this->app->scoped(IdempotencyStore::class, LaravelIdempotencyStore::class);
         $this->app->scoped(OutboxStore::class, LaravelOutboxStore::class);
