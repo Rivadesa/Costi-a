6750332111629554fc1fa7736bb5b8f1695ff9a0
Rivadesa <info@xeitoso.com>
2026-09-14 12:13:11 +0200

    style: load operator interaction refinements

--- frontend/operator/src/main.js
+++ frontend/operator/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router.js';
 import { restoreSession } from './api/client.js';
 import './style.css';
+import './operator-extra.css';
 
 await restoreSession();
 createApp(App).use(router).mount('#app');
